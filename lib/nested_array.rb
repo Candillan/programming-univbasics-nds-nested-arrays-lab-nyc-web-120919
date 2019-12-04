00e01e6259ee10adb72bdf6720391115aed6eeb0
careful-principle-0303 <cabaker96@aol.com>
2019-12-04 16:59:20 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -31,13 +31,13 @@ end
 
 def matrix_lookup(matrix, row, column)
   # Return the matrix's content at row and column
-  value = matrix[row - 1][column - 1]
+  value = matrix[row][column]
   return value
 end
 
 def matrix_update(matrix, row, column, new_value)
   # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
-  matrix[row - 1][column - 1] = new_value
+  matrix[row][column] = new_value
   matrix
 end
